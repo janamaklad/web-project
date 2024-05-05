750c4486c7c632c1928e3bb87ce251674e63dea2
samahegazy22 <sama2204043@miuegypt.edu.eg>
2024-05-05 23:03:11 +0300

    "contactus background"

---
+++ submit.js
@@ -0,0 +1,4 @@
+document.getElementById('submit-btn').addEventListener('click', function() {
+    const messageDiv = document.getElementById('message');
+    messageDiv.style.display = 'block';
+});
