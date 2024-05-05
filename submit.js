f274c12497bb26ba1f9dad15ec175bb70747db6d
samahegazy22 <sama2204043@miuegypt.edu.eg>
2024-05-05 23:26:13 +0300

    "submit.js"

--- submit.js
+++ submit.js
@@ -1,4 +1,5 @@
-document.getElementById('submit-btn').addEventListener('click', function() {
+    
+    document.getElementById('submit-btn').addEventListener('click', function() {
     const messageDiv = document.getElementById('message');
     messageDiv.style.display = 'block';
 });
