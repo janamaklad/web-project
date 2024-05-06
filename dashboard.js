c8e47f97cf6dc88765597dac2f2ab2137d302f70
JomanaAyman <jomana27904@gmail.com>
2024-05-06 23:15:47 +0300

    dashboard modified

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,12 @@
-// Assuming you have all tabs with class "tab" and all options with class "option"
-document.getElementById("content").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    var ordersContainer = document.getElementById("tab.id");
-    if (ordersContainer.style.display === "none") {
-        ordersContainer.style.display = "flex";
-    } else {
-        ordersContainer.style.display = "none";
+function manage(anchorId) {
+    // Hide all content divs
+    document.querySelectorAll('.content > div').forEach(content => {
+        content.style.display = 'none';
+    });
+
+    // Show the selected content div
+    const contentDiv = document.querySelector(`#${anchorId}`);
+    if (contentDiv) {
+        contentDiv.style.display = 'block';
     }
-});
+}
